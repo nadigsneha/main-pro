d8d26333925e3748e8fd4227d59acf11ef80ac0c
Sneha <snehanadig07@gmail.com>
2021-10-31 23:41:49 +0530

    commit

--- script.js
+++ script.js
@@ -2,25 +2,11 @@ let searchForm=document.querySelector('.login-form-container');
 document.querySelector('#login-btn'). onclick =()=>{
     searchForm.classList.toggle('active');
 }
-// let searchForm=document.querySelector('.signin-form-container');
-// document.querySelector('#signin-btn'). onclick =()=>{
-//     searchForm.classList.toggle('show');
-// }
+
 function showsignin(){
   document.querySelector('signin-form-container').style.display='block';
 }
-// function displaysignin()
-// {
-//   document.querySelector('.signin-form-container').style.display='block';
-// }
 
-
-// // When the user clicks anywhere outside of the modal, close it
-
-// let searchForm=document.querySelector('.modal');
-// document.querySelector('#start'). onclick =()=>{
-//     searchForm.classList.toggle('active');
-// }
 function displayteacher()
 {
   document.querySelector('.login-teacher').style.display='block';
